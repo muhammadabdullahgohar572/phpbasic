11042fa3c2fd97630170d3375475a85d963d4832
Muhammad Abdullah Gohar <148785694+muhammadabdullahgohar572@users.noreply.github.com>
2024-06-18 20:47:59 +0500

    Update Setcooke.php

--- Setcooke.php
+++ Setcooke.php
@@ -24,3 +24,38 @@ if (!empty($_POST)) {
 }
 
 ?>
+
+
+
+
+
+
+
+<?php
+
+
+
+if (!empty($_POST)) {
+          foreach ($_POST as $key => $value) {
+        if ($_POST['password'] == $value && $_POST['username']) {
+            $password = $_POST['password'];
+            $password=bin2hex($password);
+
+            $username = $_POST['username'];
+            $username  = bin2hex($username);
+
+            // echo $username."<br/>";
+            // echo $password . "<br/>";
+
+            setcookie($username,$password,time()+60,"/");
+            // header("Location: " . $_SERVER['PHP_SELF']);
+            // exit;
+        }
+    }
+}
+if (!empty($_COOKIE)) {
+    foreach ($_COOKIE as $key => $value) {
+        echo "Cookie for " . hex2bin($key) . ": " . hex2bin($value) . "<br/>";
+    }
+}
+?>
